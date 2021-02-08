0dea67dba91f0c7d95aa209ccf042165cb884c4c
Tyler Jewell <jewelltyler15@gmail.com>
2021-02-08 12:00:00 -0500

    fixing fks

--- grocerydb.sql
+++ grocerydb.sql
@@ -1,10 +1,10 @@
-create database grocery;
 
 use grocery;
 
 create table user(
 Id int not null primary key auto_increment,
 Name varchar(50) not null,
+shop_list_id int not null,
 foreign key (shop_list_id) references shop_list(id)
 );
 
@@ -18,13 +18,17 @@ feeds varchar(25))
 
 create table shop_list_items(
 id int not null primary key auto_increment,
+item_id int not null,
 foreign key(item_id) references item(id),
-foreign key(list_id) references shop_list(id);
+list_id int not null,
+foreign key(list_id) references shop_list(id)
+);
 
 create table shop_list(
 id int not null primary key auto_increment,
 name varchar(50) not null,
 date varchar(75) not null,
+user_id int not null,
 foreign key (user_id) references user(id)
 );
 
@@ -38,6 +42,8 @@ store varchar(25)
 
 create table recipe_items(
 id int not null primary key auto_increment,
+recipe_id int not null,
 foreign key(recipe_id) references recipe(id),
+items_id int not null,
 foreign key(items_id) references items(id)
 );
