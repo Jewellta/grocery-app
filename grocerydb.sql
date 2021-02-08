a43dbc9ec97e3d9228398def87e7fac0c2c5d616
Tyler Jewell <jewelltyler15@gmail.com>
2021-02-08 16:28:44 -0500

    end of day

--- grocerydb.sql
+++ grocerydb.sql
@@ -1,16 +1,30 @@
+use sys;
+
+drop database if exists grocery;
+
+create database grocery;
 
 use grocery;
+
+create table shop_list(
+id int not null primary key auto_increment,
+name varchar(50) not null,
+date varchar(75) not null,
+user_id int not null,
+foreign key (user_id) references user(id)
+);
 
 create table user(
 Id int not null primary key auto_increment,
 Name varchar(50) not null,
 shop_list_id int not null,
-foreign key (shop_list_id) references shop_list(id)
+	foreign key (shop_list_id) references shop_list(id)
 );
 
 create table recipe(
 id int not null primary key auto_increment,
-items varchar(100) not null,
+description varchar(500) not null,
+items varchar(1000) not null,
 cook_time int(5) not null,
 appliances varchar(100) not null,
 feeds varchar(25))
@@ -24,13 +38,6 @@ list_id int not null,
 foreign key(list_id) references shop_list(id)
 );
 
-create table shop_list(
-id int not null primary key auto_increment,
-name varchar(50) not null,
-date varchar(75) not null,
-user_id int not null,
-foreign key (user_id) references user(id)
-);
 
 create table items(
 id int not null primary key auto_increment,
